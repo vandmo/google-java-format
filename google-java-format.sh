a3e59b3c067872cd37b78e1a4a10a5092626a487
Mikael Vandmo <mikael@vandmo.se>
2022-05-30 21:13:39 +0200

    --add-exports should not be needed anymore

--- google-java-format.sh
+++ google-java-format.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 set -eu -o pipefail
 
-java \
- --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
- --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
- --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
- --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
- --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
- -jar "${GOOGLE_JAVA_FORMAT_JAR_ABSOLUTE_PATH}" "$@"
+java -jar "${GOOGLE_JAVA_FORMAT_JAR_ABSOLUTE_PATH}" "$@"
