c321a290a9776b1fb712674123b82d0bbc4d1514
Mikael Vandmo <mikael@vandmo.se>
2026-07-18 10:00:47 +0200

    Add sample Java file for testing

---
+++ test-sample.java
@@ -0,0 +1,11 @@
+public class TestSample {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+
+  public void testMethod() {
+    int x=1;
+    int y  =  2;
+    System.out.println(x+y);
+  }
+}
